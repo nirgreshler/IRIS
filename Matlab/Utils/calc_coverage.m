f6d5b5eaf0f514a30b462632e36d38ad1dd8f93b
Nir <nirgreshler@gmail.com>
2020-03-14 11:29:26 +0200

    minor bug fix in calc_coverage

--- Matlab/Utils/calc_coverage.m
+++ Matlab/Utils/calc_coverage.m
@@ -3,6 +3,6 @@ function cov_set = calc_coverage(G, pathId)
 cov_set = [];
 for i = 1:length(pathId)
     cov = G.graph.Nodes.vis{G.graph.Nodes.id == pathId(i)};
-    cov_set = [cov_set cov];
+    cov_set = [cov_set cov(~isnan(cov))];
 end
 cov_set = unique(cov_set) + 1;
